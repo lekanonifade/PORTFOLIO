7e0b827f08be7250e85512a9a7a49a91a7455f26
Olamilekan <ayomikunolamilekan@gmail.com>
2023-09-28 10:55:28 +0100

    update

--- src/components/links/linkData.js
+++ src/components/links/linkData.js
@@ -1,36 +1,35 @@
 export default [
     {
-        // linkImage: "fa-brands fa-discord",
         linkImage: "../images/link/link-discord.svg",
         linkText: "Discord",
         linkHref: ""
     },
     {
-        // linkImage: "fa-brands fa-facebook-f",
-        linkImage: "../images/link/link-facebook.svg",
-        linkText: "Facebook",
-        linkHref: "https://web.facebook.com/profile.php?id=100091869093659"
+        linkImage: "../images/link/link-instagram.svg",
+        linkText: "Instagram",
+        linkHref: "https://github.com/wildaffod"
     },
     {
-        // linkImage: "fa-brands fa-github",
+        linkImage: "../images/link/link-linkedin.svg",
+        linkText: "LinkedIn",
+        linkHref: "https://github.com/wildaffod"
+    },
+    {
         linkImage: "../images/link/link-github.svg",
         linkText: "Github",
         linkHref: "https://github.com/wildaffod"
     },
     {
-        // linkImage: "fa-brands fa-twitter",
         linkImage: "../images/link/link-twitter.svg",
         linkText: "Twitter",
         linkHref: "https://twitter.com/wildaffod"
     },
     {
-        // linkImage: "fa-brands fa-whatsapp",
         linkImage: "../images/link/link-whatsapp.svg",
         linkText: "WhatsApp",
         linkHref: "https://wa.me/2348034332268"
     },
     {
-        // linkImage: "fa-solid fa-envelope link-envelope",
         linkImage: "../images/link/link-gmail.svg",
         linkText: "Gmail",
         linkHref: "mailto:ayomikunolamilekan@gmail.com"
