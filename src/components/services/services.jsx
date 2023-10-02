25e37533f027f228d25ebb691b85334fc8f330eb
Olamilekan <ayomikunolamilekan@gmail.com>
2023-10-02 12:45:47 +0100

    update

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -8,17 +8,17 @@ export default function Services() {
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae culpa eaque, eveniet ratione fugiat temporibus, eligendi amet beatae repellendus accusantium error distinctio ipsa provident, minus impedit. Non, doloribus! Cumque, nulla.</p>
             <div className="services--grid">
                 <div className="services first--service">
-                    <img src="" alt="" />
+                    <img src="/images/services/services-webdev.svg" alt="" />
                     <h4>Web Development</h4>
                     <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
                 <div className="services second--service">
-                    <img src="" alt="" />
+                    <img src="/images/services/services-webdes.svg" alt="" />
                     <h4>Web Design</h4>
                     <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
                 <div className="services third--service">
-                    <img src="" alt="" />
+                    <img src="/images/services/services-support.svg" alt="" />
                     <h4>Support</h4>
                     <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
