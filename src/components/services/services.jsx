dd9ec80c77a1ac19dc5a45784b1b2ff51685e739
Olamilekan <ayomikunolamilekan@gmail.com>
2023-10-03 00:10:31 +0100

    update

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -5,24 +5,28 @@ export default function Services() {
     return (
         <div className="section services--section">
             <p className="title services--title web--title">Services</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae culpa eaque, eveniet ratione fugiat temporibus, eligendi amet beatae repellendus accusantium error distinctio ipsa provident, minus impedit. Non, doloribus! Cumque, nulla.</p>
             <div className="services--grid">
                 <div className="services first--service">
-                    <img src="/images/services/services-webdev.svg" alt="" />
-                    <h4>Web Development</h4>
-                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <img src="/images/services/services-webdev.svg" alt="" className="services--image" />
+                    <h4 className="services--title">Web Development</h4>
                 </div>
                 <div className="services second--service">
-                    <img src="/images/services/services-webdes.svg" alt="" />
-                    <h4>Web Design</h4>
-                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <img src="/images/services/services-webdes.svg" alt="" className="services--image" />
+                    <h4 className="services--title">Web Design</h4>
                 </div>
                 <div className="services third--service">
-                    <img src="/images/services/services-support.svg" alt="" />
-                    <h4>Support</h4>
-                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <img src="/images/services/services-support.svg" alt="" className="services--image" />
+                    <h4 className="services--title">Support</h4>
                 </div>
             </div>
+
+            <p className="services--about">
+                I provide a comprehensive range of services to enhance your online presence.
+                My web development expertise transforms your ideas into functional and visually appealing websites, focusing on user engagement.
+                I also specialize in crafting stunning web designs that reflect your brand and offer an exceptional user experience.
+                In addition, I offer vital website support services to keep your online platform secure, up-to-date, and optimized for peak performance.
+                With my technical expertise and proactive monitoring, your website remains in top shape while you focus on your core business.
+            </p>
         </div>
     )
 }
