10ebcc6fd0de8cfc7a412e73ad1ae09d0d7bbede
Olamilekan <ayomikunolamilekan@gmail.com>
2023-10-03 13:12:57 +0100

    update

--- src/components/links/linkProp.jsx
+++ src/components/links/linkProp.jsx
@@ -1,11 +1,10 @@
 export default function Linkprop(props) {
     return (
         <div>
-            <a href={props.linkHref} className="link--container">
-                {/* <p className="link--image"><i className={props.linkImage}></i></p> */}
+            <div className="link--container">
                 <img src={props.linkImage} className="link--image"/>
-                <p className="link--text">{props.linkText}</p>
-            </a>
+                <p><a href={props.linkHref}  className="link--text">{props.linkText}</a></p>
+            </div>
         </div>
     )
 }
